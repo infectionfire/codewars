a95b7dacee340e24718c2e684aa4a1a0aca3e5cb
Infectionfire <infectionfire0911@gmail.com>
2021-12-09 23:16:35 +0300

    add LineNumbers Kata

---
+++ level7/level7/LineNumbers.java
@@ -0,0 +1,24 @@
+package level7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class LineNumbers {
+
+    public static List<String> number(List<String> lines) {
+        List<String> result = new ArrayList<String>();
+        for ( int i=1; i<lines.size()+1; i++ ) {
+            result.add( String.valueOf(i)+": "+lines.get(i-1) );
+        }
+        return  result;
+    }
+
+    public static List<String> number2(List<String> lines) {
+        AtomicInteger i = new AtomicInteger(1);
+        return lines.stream().map(e -> i.getAndIncrement() + ": " + e).collect(Collectors.toList());
+
+    }
+
+}
